22d582d2ef035b6088d5f278656063d8dccc8123
NatalliaPahosava <mulato4ka114@gmail.com>
2022-07-21 21:10:20 -0400

    add some aditions

--- scrypts.js
+++ scrypts.js
@@ -1,39 +1,35 @@
-let a='';
-let b='';
-let numbers=[];
-let res;
+let a = ''
 
-function numOperations(num){
-    numbers.push(num);
-    if(numbers.length !=1){
-        a = '';
-document.getElementById('display').innerHTML= a
-    }
-    for(let i=0; i<numbers.length; i++) 
-    {
-        a=a+numbers[i]
-    }
-    document.getElementById('display').innerHTML=a
+let numbers = []
+let res
+function numOperations(num) {
+  numbers.push(num)
+  if (numbers.length) {
+    a = ''
+    document.getElementById('display').innerHTML = a
+  }
+  for (let i = 0; i < numbers.length; i++) {
+    a = a + numbers[i]
 
+  }
+  document.getElementById('display').innerHTML = a
 }
 
-function result(){
-    document.getElementById('display').innerHTML='';
-    for (i=0; i<numbers.length; i++){
-        b+=numbers[i]
-    }
-    res=eval(b);
-    document.getElementById('display').innerHTML=res;
-    while(numbers.length>0){
-        numbers.pop();
-    }
-    numbers.push(res.toString());
+function result() {
+  document.getElementById('display').innerHTML = ''
+  res = eval(a)
+
+  document.getElementById('display').innerHTML = res
+  while (numbers.length > 0) {
+    numbers.pop()
+  }
+
 }
-function clr(){
-    document.getElementById('display').innerHTML = "0";
-    while(numbers.length>0){
-        numbers.pop();
-    }
-    a="";
-    b="";
+function clr() {
+  document.getElementById('display').innerHTML = '0'
+  while (numbers.length > 0) {
+    numbers.pop()
+  }
+  a = ''
+
 }
